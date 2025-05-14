e5efc11ef31d850684b69167b0352118b765fc95
Julien JAMET <julienjamet1992@protonmail.com>
2025-05-14 02:10:58 +0200

    configure preview network binding and allowed hosts for production

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,31 @@
 /****************************************************************IMPORTS*/
 /****************************************NPM MODULES*/
+import dotenv from 'dotenv';
 import { UserConfig, defineConfig } from 'vite';
+/****************************************************/
+
+/*****************************************INTERFACES*/
+import { IPreview, IViteConfigOptions } from './src/interfaces/config/config.ts';
 /****************************************************/
 /************************************************************************/
 
 
 /************************************************************VITE CONFIG*/
-const viteConfig: UserConfig = defineConfig({});
+dotenv.config();
+
+/*************************************PREVIEW CONFIG*/
+const preprodUrl: string = process.env.VITE_HUB_PREPROD_URL || '';
+
+const preview: IPreview = {
+    allowedHosts: [preprodUrl]
+};
+/****************************************************/
+
+const viteConfigOptions: IViteConfigOptions = {
+    preview: preview
+};
+
+const viteConfig: UserConfig = defineConfig(viteConfigOptions);
 
 export default viteConfig;
 /************************************************************************/

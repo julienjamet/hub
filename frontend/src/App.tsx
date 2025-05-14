b690e05a678ebcb6c44902f8c3298ed459b42d87
Julien JAMET <julienjamet1992@protonmail.com>
2025-05-14 05:45:59 +0200

    add navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
 /****************************************************************IMPORTS*/
 /****************************************NPM MODULES*/
 import { FC, ReactElement } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+/****************************************************/
+
+/*****************************************COMPONENTS*/
+import { Navbar } from './components/Navbar.tsx';
+import { Projects } from './components/Projects.tsx';
 /****************************************************/
 /************************************************************************/
 
@@ -9,8 +15,32 @@ import { FC, ReactElement } from 'react';
 export const App: FC = (): ReactElement => {
     /*****************************************RETURN*/
     return (
-        <>
-        </>
+        <BrowserRouter>
+            <Navbar />
+
+            <Routes>
+                <Route
+                    path='/training'
+                    element={
+                        <Projects />
+                    }
+                />
+
+                <Route
+                    path='/projects'
+                    element={
+                        <Projects />
+                    }
+                />
+
+                <Route
+                    path='*'
+                    element={
+                        <Navigate replace to='/' />
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
     );
     /************************************************/
 };
